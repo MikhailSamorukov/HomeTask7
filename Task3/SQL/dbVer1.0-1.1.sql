e67d6c1ed28de629202cc3b7eeede87bf9b13a5b
Mihail Samorukov <Misha_Sam@mail.ru>
2018-02-25 13:59:51 +0600

    Finished

---
+++ Task3/SQL/dbVer1.0-1.1.sql
@@ -0,0 +1,13 @@
+﻿if OBJECT_ID('EmployeeCreditCards') is null
+begin
+	CREATE TABLE [dbo].[EmployeeCreditCards] (
+		[CardNumber]         INT        NOT NULL,
+		[CardExpirationDate] DATETIME   NOT NULL,
+		[CardHolder]         NCHAR (50) NOT NULL,
+		[EmployeeId]         INT        NOT NULL,
+		CONSTRAINT [PK_EmployeeCreditCards] PRIMARY KEY CLUSTERED ([CardNumber] ASC)
+	);
+
+	ALTER TABLE [dbo].[EmployeeCreditCards] WITH NOCHECK
+		ADD CONSTRAINT [FK_EmployeeCreditCards_Employees] FOREIGN KEY ([EmployeeId]) REFERENCES [dbo].[Employees] ([EmployeeID]);
+end
---
+++ Task3/SQL/dbVer1.1-1.3.sql
@@ -0,0 +1,5 @@
+﻿ IF COL_LENGTH('Customers','FiundationDate') IS NULL
+ BEGIN
+	ALTER TABLE [dbo].[Customers]
+		ADD [FiundationDate] DATETIME NULL;
+ END
